85f57ddb265b8af7a3082f7c3384a7046e0e8e00
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-01 19:42:11 +0200

    Add decorators

--- person.rb
+++ person.rb
@@ -13,9 +13,46 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
+  end
+end
+
+# Step 1: Create the Nameable class
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'You must implement the correct_name method.'
+  end
+end
+
+# Step 2: Create the Decorator base class
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+# Step 3: Create the CapitalizeDecorator class
+class CapitalizeDecorator < Decorator
+  def correct_name
+    super.capitalize
+  end
+end
+
+# Step 4: Create the TrimmerDecorator class
+class TrimmerDecorator < Decorator
+  def correct_name
+    super[0..9]
   end
 end
